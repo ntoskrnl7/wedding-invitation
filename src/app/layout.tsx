a2bf1fd014936c479e7dfcaeaa1427acff77be5c
Jung-Kwang Lee <ntoskrnl7@gmail.com>
2024-04-25 10:38:45 +0900

    제목 변경.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import { ReactNode } from "react";
+import type { Metadata } from 'next';
+import { ReactNode } from 'react';
 
 import theme from './theme';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 
 export const metadata: Metadata = {
-  title: "하은 ❤ 중광 웨딩",
-  description: "웨딩 초대장",
+  title: '하은 ❤ 중광 결혼식에 초대합니다',
+  description: '하은 ❤ 중광 결혼식에 초대합니다',
 };
 
 import './palette.global.scss';
 import './global.scss';
 
-import { MenuStateProvider } from "./menu";
+import { MenuStateProvider } from './menu';
 
 export default function RootLayout({
   children,
@@ -21,12 +21,12 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang='ko'>
       <head>
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=yes" />
-        <link rel="manifest" href="./manifest.json" />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=yes' />
+        <link rel='manifest' href='./manifest.json' />
       </head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
